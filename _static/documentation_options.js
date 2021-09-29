c34efd794cb44a34326f4766af0adcc9ada9cf01
mysticfall <mysticfall@users.noreply.github.com>
2021-09-29 03:05:35 +0000

    deploy: UPBGE/upbge@a5ace7d386974f5a157939760ca32ee79075ee68

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '6234ad682e95',
+    VERSION: 'a5ace7d38697',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
