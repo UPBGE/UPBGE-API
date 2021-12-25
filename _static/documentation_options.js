b10f55284ef4c030ea217e7e72d01ea837af7677
lordloki <lordloki@users.noreply.github.com>
2021-12-25 12:48:19 +0000

    deploy: UPBGE/upbge@ae73a5e4b195cb2ba5e4cb99c9083555b1d04fcd

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,6 @@
 var DOCUMENTATION_OPTIONS = {
     URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
-    VERSION: '51cd0bfe33ff',
+    VERSION: 'ae73a5e4b195',
     LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
