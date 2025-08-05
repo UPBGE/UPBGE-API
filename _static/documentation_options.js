89303a1b72f8bd2ff3625c4751ed2e45d8b31e2c
Jorge Bernal <jbernalmartinez@gmail.com>
2025-08-05 23:53:18 +0200

    deploy: UPBGE/upbge@8fe9080

--- _static/documentation_options.js
+++ _static/documentation_options.js
@@ -1,6 +1,7 @@
-const DOCUMENTATION_OPTIONS = {
-    VERSION: '4.5',
-    LANGUAGE: 'en',
+var DOCUMENTATION_OPTIONS = {
+    URL_ROOT: document.getElementById("documentation_options").getAttribute('data-url_root'),
+    VERSION: '5.0',
+    LANGUAGE: 'None',
     COLLAPSE_INDEX: false,
     BUILDER: 'html',
     FILE_SUFFIX: '.html',
